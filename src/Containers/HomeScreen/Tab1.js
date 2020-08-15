1138f049cfac5990b68072b9074b5d918c969808
thomc <thomc.it@gmail.com>
2020-08-15 11:17:38 +0700

    edit icon and language

--- src/Containers/HomeScreen/Tab1.js
+++ src/Containers/HomeScreen/Tab1.js
@@ -1,102 +1,173 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { NavHeader, Header } from "../../Components/Header";
 import { Colors, UtillSize } from "../../Themes";
 import { Images } from "../../Themes";
 import { Icon } from "native-base";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  StyleSheet
-} from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 
 function Tab1({ navigation }) {
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <View style={{ marginHorizontal: 10, marginTop:10 }}>
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Image source={Images.ImageFarmer} style={{ width: 45, height: 45, borderRadius: 45, marginRight: 10 }} />
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={{ marginHorizontal: 10, marginTop: 10 }}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            source={Images.ImageFarmer}
+            style={{
+              width: 45,
+              height: 45,
+              borderRadius: 45,
+              marginRight: 10,
+            }}
+          />
           <View>
-            <Text style={{ fontWeight: 'bold', fontSize: UtillSize.titleFontSize }}>Mai Chi Tho</Text>
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: UtillSize.titleFontSize,
+              }}
+            >
+              Mai Chi Tho
+            </Text>
             <Text style={{ color: Colors.mainColor }}>Nông trại Hậu Giang</Text>
           </View>
         </View>
         <View style={{ marginTop: 25 }}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('RequestPrescription')}>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("RequestPrescription")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageOrderRequest} style={styles.imageIcon} />
+                <Image
+                  source={Images.ImageOrderRequest}
+                  style={styles.imageIcon}
+                />
               </View>
-              <Text style={styles.ItemText}>Tạo đề nghị đơn thuốc</Text>
+              <Text style={styles.ItemText}>Đề nghị {"\n"} Đơn thuốc</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('DsDeNghiScreen')}>
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("DsDeNghiScreen")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageBusinessReport} style={styles.imageIcon} />
+                <Image
+                  source={Images.ImageRequestList}
+                  style={styles.imageIcon}
+                />
               </View>
-              <Text style={styles.ItemText}>Danh Sách đề nghị</Text>
+              <Text style={styles.ItemText}>Danh sách {"\n"} Đề nghị</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('NhapKhoTheoDonThuocScreen')}>
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("DsChoNhanHangScreen")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageNewProduct} style={styles.imageIcon} />
+                <Image source={Images.ImageDelivery} style={styles.imageIcon} />
+              </View>
+              <Text style={styles.ItemText}>Hàng chờ {"\n"} về kho</Text>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 10,
+            }}
+          >
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("NhapKhoTheoDonThuocScreen")}
+            >
+              <View style={styles.WrapIcon}>
+                <Image
+                  source={Images.ImageNewProduct}
+                  style={styles.imageIcon}
+                />
               </View>
               <Text style={styles.ItemText}>Nhập kho</Text>
             </TouchableOpacity>
 
-          </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('TheKhoScreen')}>
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("TaoPhieuXuatKhoScreen")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageReportCard} style={styles.imageIcon} />
+                <Image
+                  source={Images.ImageExportStock}
+                  style={styles.imageIcon}
+                />
               </View>
-              <Text style={styles.ItemText}>Thẻ kho</Text>
+              <Text style={styles.ItemText}>Xuất kho</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('DsChoNhanHangScreen')}>
-              <View style={styles.WrapIcon}>
-                <Image source={Images.ImageBusinessReport} style={styles.imageIcon} />
-              </View>
-              <Text style={styles.ItemText}>Danh sách chờ nhận hàng</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('DsPhieuNhapXuatKhoScreen')}>
+
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("DsPhieuNhapXuatKhoScreen")}
+            >
               <View style={styles.WrapIcon}>
                 <Image source={Images.ImageOrder} style={styles.imageIcon} />
               </View>
-              <Text style={styles.ItemText}>Phiếu nhập, xuất kho</Text>
+              <Text style={styles.ItemText}>Phiếu {"\n"} Nhập, Xuất</Text>
             </TouchableOpacity>
-
           </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('TaoPhieuXuatKhoScreen')}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 10,
+            }}
+          >
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("TheKhoScreen")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageExportStock} style={styles.imageIcon} />
+                <Image
+                  source={Images.ImageReportCard}
+                  style={styles.imageIcon}
+                />
               </View>
-              <Text style={styles.ItemText}>Tạo phiếu xuất kho</Text>
+              <Text style={styles.ItemText}>Thẻ kho</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('TonKhoScreen')}>
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("TonKhoScreen")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageWareHouse} style={styles.imageIcon} />
+                <Image
+                  source={Images.ImageWareHouse}
+                  style={styles.imageIcon}
+                />
               </View>
               <Text style={styles.ItemText}>Tồn Kho</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('BaoCaoScreen')}>
+            <TouchableOpacity
+              style={styles.itemMenu}
+              onPress={() => navigation.navigate("BaoCaoScreen")}
+            >
               <View style={styles.WrapIcon}>
-                <Image source={Images.ImageBusinessReport} style={styles.imageIcon} />
+                <Image
+                  source={Images.ImageBusinessReport}
+                  style={styles.imageIcon}
+                />
               </View>
-              <Text style={styles.ItemText}>Báo cáo tổng hợp</Text>
+              <Text style={styles.ItemText}>Báo cáo</Text>
             </TouchableOpacity>
           </View>
         </View>
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   itemMenu: {
-    width: '30%',
+    width: "30%",
     paddingHorizontal: 3,
-    // justifyContent:'center', 
-    alignItems: 'center',
+    // justifyContent:'center',
+    alignItems: "center",
     paddingVertical: 8,
     borderRadius: 8,
     backgroundColor: "#fff",
@@ -105,24 +176,24 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     shadowOffset: {
       height: 1,
-      width: 0
-    }
+      width: 0,
+    },
   },
   ItemIcon: {
-    color: Colors.mainColor
+    color: Colors.mainColor,
   },
   ItemText: {
     fontSize: UtillSize.smallFontSize,
     color: Colors.ColorNhat,
-    textAlign: 'center',
-    textAlignVertical: 'center'
+    textAlign: "center",
+    textAlignVertical: "center",
   },
   WrapIcon: {
     height: 40,
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: "center",
   },
-  imageIcon: { width: 40, height: 40 }
-})
+  imageIcon: { width: 40, height: 40 },
+});
 
 export default Tab1;
--- src/Containers/RequestPrescription/RequestPrescription.js
+++ src/Containers/RequestPrescription/RequestPrescription.js
@@ -1,87 +1,90 @@
 import React, { useState, useEffect, Component } from "react";
 // import styles from "./DrawerStyle";
 import { NavHeader, Header } from "../../Components/Header";
-import {
-    Text,
-    View
-} from "react-native";
-import { Item, Input, Label } from 'native-base';
+import { Text, View } from "react-native";
+import { Item, Input, Label } from "native-base";
 import { Button } from "../../Components/Button";
 import { SelectComponent } from "../../Components/Select";
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import HomeScreen from "../HomeScreen/HomeScreen";
 
-
-class RequestPrescription extends Component  {
-    constructor(props) {
-        super(props);
-        console.log('dddddda',this.props.tab);
-
-    }
-render() {
+class RequestPrescription extends Component {
+  constructor(props) {
+    super(props);
+    console.log("dddddda", this.props.tab);
+  }
+  render() {
     return (
-        <View style={{ flex: 1, backgroundColor: '#fff' }}>
-            <Header
-                leftFunction={() => this.props.navigation.navigate("HomeScreen")}
-                IconLeft={this.props.tab?null:{ name: "arrow-back", type: "Ionicons" }}
-                title={"Request for prescription"}
-                NoNavHeader={this.props.tab?? false}
-            />
-            <View style={{ flex: 1 }}>
-                <KeyboardAwareScrollView extraScrollHeight={50}>
-                    <View style={{ paddingVertical: 20, alignItems: 'center', backgroundColor: '#eaeaea', }}>
-                        <Text>FARM OWNER INFORMATION</Text>
-                    </View>
-                    <View>
-                        <View style={{ paddingVertical: 5 }}>
-                            <Item floatingLabel>
-                                <Label>Farm name</Label>
-                                <Input value={'Nông trại Hậu Giang'}/>
-                            </Item>
-                        </View>
-                
-                    </View>
-                    <View style={{ paddingVertical: 20, alignItems: 'center', backgroundColor: '#eaeaea', }}>
-                        <Text>SICK ANIMAL INFORMATION</Text>
-                    </View>
-                    <View>
-                        <View style={{ paddingVertical: 5 }}>
-                            <Item floatingLabel>
-                                <Label>Animal description</Label>
-                                <Input />
-                            </Item>
-                        </View>
-                        <View style={{ paddingVertical: 5 }}>
-                            <Item floatingLabel>
-                                <Label>Amount</Label>
-                                <Input />
-                            </Item>
-                        </View>
-                        <View style={{ paddingVertical: 5 }}>
-                            <Item floatingLabel>
-                                <Label>Age</Label>
-                                <Input />
-                            </Item>
-                        </View>
-                        <View style={{ paddingVertical: 5 }}>
-                            <Item floatingLabel>
-                                <Label>Species</Label>
-                                <Input />
-                            </Item>
-                        </View>
-                    </View>
-                    {/* <View style={{ paddingVertical: 20, alignItems: 'center', backgroundColor: '#eaeaea', }}>
+      <View style={{ flex: 1, backgroundColor: "#fff" }}>
+        <Header
+          leftFunction={() => this.props.navigation.navigate("HomeScreen")}
+          IconLeft={
+            this.props.tab ? null : { name: "arrow-back", type: "Ionicons" }
+          }
+          title={"Yêu cầu đơn thuốc"}
+          NoNavHeader={this.props.tab ?? false}
+        />
+        <View style={{ flex: 1 }}>
+          <KeyboardAwareScrollView extraScrollHeight={50}>
+            <View>
+              <View style={{ paddingVertical: 5 }}>
+                <Item floatingLabel>
+                  <Label>Tên trang trại</Label>
+                  <Input value={"Nông trại Hậu Giang"} />
+                </Item>
+              </View>
+            </View>
+            <View
+              style={{
+                paddingVertical: 20,
+                alignItems: "center",
+                backgroundColor: "#eaeaea",
+              }}
+            >
+              <Text>Thông tin đàn động vật</Text>
+            </View>
+            <View>
+              <View style={{ paddingVertical: 5 }}>
+                <Item floatingLabel>
+                  <Label>Mô tả</Label>
+                  <Input />
+                </Item>
+              </View>
+              <View style={{ paddingVertical: 5 }}>
+                <Item floatingLabel>
+                  <Label>Số lượng</Label>
+                  <Input />
+                </Item>
+              </View>
+              <View style={{ paddingVertical: 5 }}>
+                <Item floatingLabel>
+                  <Label>Tuổi</Label>
+                  <Input />
+                </Item>
+              </View>
+              <View style={{ paddingVertical: 5 }}>
+                <Item floatingLabel>
+                  <Label>Mô tả bệnh</Label>
+                  <Input />
+                </Item>
+              </View>
+            </View>
+            {/* <View style={{ paddingVertical: 20, alignItems: 'center', backgroundColor: '#eaeaea', }}>
                         <Text>SICK ANIMAL INFORMATION</Text>
                     </View> */}
-                    <View style={{marginVertical: 20,}}>
-                        <Button title='Next' onPress={() => this.props.navigation.navigate('DsSanPhamScreen')}/>
-                    </View>
-                </KeyboardAwareScrollView>
+            <View style={{ marginVertical: 20 }}>
+              <Button
+                title="Next"
+                onPress={() =>
+                  this.props.navigation.navigate("DsSanPhamScreen")
+                }
+              />
             </View>
+          </KeyboardAwareScrollView>
         </View>
-    )
-}
-    
+      </View>
+    );
+  }
 }
 
 export default RequestPrescription;
